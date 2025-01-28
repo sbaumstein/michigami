e6027853889467b89029524bf919b934285f452d
sbaumstein <sambaum@umich.edu>
2025-01-27 23:04:38 -0500

    cleaned up DB and implemented pandas

--- Loaders/football_loader.py
+++ Loaders/football_loader.py
@@ -12,6 +12,141 @@ def scrape_historical():
     years = soup.find_all('table')
     year_data = {}
 
+    # 2024 hardcoded data
+    games2024 = {}
+
+    game = {
+        'opponent_name': "Fresno State",
+        'conference_game': False,
+        'date': '8/31/2024',
+        'michigan_score': '30',
+        'opponent_score': '10',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Texas",
+        'conference_game': False,
+        'date': '9/7/2024',
+        'michigan_score': '12',
+        'opponent_score': '31',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Arkansas State",
+        'conference_game': False,
+        'date': '9/14/2024',
+        'michigan_score': '28',
+        'opponent_score': '18',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Southern California",
+        'conference_game': True,
+        'date': '9/21/2024',
+        'michigan_score': '27',
+        'opponent_score': '24',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Minnesota",
+        'conference_game': True,
+        'date': '9/28/2024',
+        'michigan_score': '27',
+        'opponent_score': '24',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Washington",
+        'conference_game': True,
+        'date': '10/5/2024',
+        'michigan_score': '17',
+        'opponent_score': '27',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Illinois",
+        'conference_game': True,
+        'date': '10/19/2024',
+        'michigan_score': '7',
+        'opponent_score': '21',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Michigan State",
+        'conference_game': True,
+        'date': '10/26/2024',
+        'michigan_score': '24',
+        'opponent_score': '27',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Oregon",
+        'conference_game': True,
+        'date': '11/2/2024',
+        'michigan_score': '38',
+        'opponent_score': '17',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Indiana",
+        'conference_game': True,
+        'date': '11/9/2024',
+        'michigan_score': '15',
+        'opponent_score': '20',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Northwestern",
+        'conference_game': True,
+        'date': '11/23/2024',
+        'michigan_score': '50',
+        'opponent_score': '6',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Ohio State",
+        'conference_game': True,
+        'date': '11/30/2024',
+        'michigan_score': '13',
+        'opponent_score': '10',
+        'event_type': 'Regular Season Game'
+    }
+    games2024[game['opponent_name']] = game
+
+    game = {
+        'opponent_name': "Alabama",
+        'conference_game': False,
+        'date': '12/31/2024',
+        'michigan_score': '19',
+        'opponent_score': '13',
+        'event_type': 'ReliaQuest Bowl'
+    }
+    games2024[game['opponent_name']] = game
+
+    year_data['2024'] = games2024
+
     # scraping all historical data from 1892-2013
     for year in years[1:-2]:
         season = year.find('a').text[:4]
@@ -25,10 +160,10 @@ def scrape_historical():
                 if value.find('a') != None:
                     opponent_name = value.find('a').text
                     game_stats['opponent_name'] = opponent_name
-                    game_stats['conference_game'] = True
+                    game_stats['conference_game'] = False
                     if game_stats['opponent_name'].startswith('*'):
                         game_stats['conference_game'] = True
-                        game_stats['opponent_name'] = game_stats['opponent_name'][1:]
+                        opponent_name = game_stats['opponent_name'][1:]
             if opponent_name == None:
                 game_stats['opponent_name'] = values[2].text
                 opponent_name = values[2].text
@@ -47,117 +182,8 @@ def scrape_historical():
             game_data[opponent_name] = game_stats
 
         year_data[season] = game_data
-    
-    #2024 hard coded data
-    games2024 = {}
-    game = {}
-    game['opponent_name'] = "Fresno State"
-    game['conference_game'] = False
-    game['date'] = '8/31/2024'
-    game['michigan_score'] = '30'
-    game['opponent_score'] = '10'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
 
-    game['opponent_name'] = "Texas"
-    game['conference_game'] = False
-    game['date'] = '9/7/2024'
-    game['michigan_score'] = '12'
-    game['opponent_score'] = '31'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Arkansas State"
-    game['conference_game'] = False
-    game['date'] = '9/14/2024'
-    game['michigan_score'] = '28'
-    game['opponent_score'] = '18'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Southern California"
-    game['conference_game'] = True
-    game['date'] = '9/21/2024'
-    game['michigan_score'] = '27'
-    game['opponent_score'] = '24'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Minnesota"
-    game['conference_game'] = True
-    game['date'] = '9/28/2024'
-    game['michigan_score'] = '27'
-    game['opponent_score'] = '24'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Washington"
-    game['conference_game'] = True
-    game['date'] = '10/5/2024'
-    game['michigan_score'] = '17'
-    game['opponent_score'] = '27'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Illinois"
-    game['conference_game'] = True
-    game['date'] = '10/19/2024'
-    game['michigan_score'] = '7'
-    game['opponent_score'] = '21'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Michigan State"
-    game['conference_game'] = True
-    game['date'] = '10/26/2024'
-    game['michigan_score'] = '24'
-    game['opponent_score'] = '27'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Oregon"
-    game['conference_game'] = True
-    game['date'] = '11/2/2024'
-    game['michigan_score'] = '38'
-    game['opponent_score'] = '17'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Indiana"
-    game['conference_game'] = True
-    game['date'] = '11/9/2024'
-    game['michigan_score'] = '15'
-    game['opponent_score'] = '20'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Northwestern"
-    game['conference_game'] = True
-    game['date'] = '11/23/2024'
-    game['michigan_score'] = '50'
-    game['opponent_score'] = '6'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Ohio State"
-    game['conference_game'] = True
-    game['date'] = '11/30/2024'
-    game['michigan_score'] = '13'
-    game['opponent_score'] = '10'
-    game['event_type'] = 'Regular Season Game'
-    games2024[game['opponent_name']] = game
-
-    game['opponent_name'] = "Alabama"
-    game['conference_game'] = False
-    game['date'] = '12/31/2024'
-    game['michigan_score'] = '19'
-    game['opponent_score'] = '13'
-    game['event_type'] = 'ReliaQuest Bowl'
-    games2024[game['opponent_name']] = game
-
-
-    year_data['2024'] = games2024
-    
+        
     return year_data
 
 def load_db(db_file, year_data):
@@ -169,7 +195,7 @@ def load_db(db_file, year_data):
     # Insert each game's data into the database
     for season, games in year_data.items():
         for opponent_name, game_stats in games.items():
-            game_date = datetime.strptime(game_stats['date'], "%m/%d/%Y")
+            game_date = datetime.strptime(game_stats['date'], "%m/%d/%Y").date()
             try:
                 cursor.execute("""
                 INSERT INTO football (MichiganScore, OpponentScore, OpponentName, EventType, ConferenceGame, GameDate)
@@ -192,7 +218,7 @@ def load_db(db_file, year_data):
 def main():
     """ Main Function """
 
-    db_file = "../SQL/michigami.db"
+    db_file = "michigami.db"
     print("Scraping Michigan football data...")
     year_data = scrape_historical()
     load_db(db_file, year_data)
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import time
 import requests
 import sqlite3
+import pandas as pd
 
 def scrape_scores():
-    # Your scraping logic goes here
-    pass
+    # Example: Reading from SQLite to Pandas
+    connection = sqlite3.connect("michigami.db")
+    df = pd.read_sql_query("SELECT * FROM football", connection)
+    reverse = df.iloc[::-1].reset_index(drop=True)
+    unique_scores = reverse.drop_duplicates(subset=["MichiganScore", "OpponentScore"]).reset_index(drop=True)
+    print(unique_scores.tail(18))
+
+    connection.close()
 
 def main():
-    while True:
-        scrape_scores()
-        time.sleep(60 * 5)  # Sleep for 5 minutes before the next scrape
+    scrape_scores()
 
 if __name__ == "__main__":
     main()
